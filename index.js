0ce49af1629942ee3773e703664b7110736b2d4b
ZeroBug <404.zerobug@gmail.com>
2022-05-19 13:30:35 +0600

    added jwt for users

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require("dotenv").config();
+var jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
@@ -12,11 +13,28 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pyeyw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJwt (req, res, next) {
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message: 'UnAuthorized'})
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+    if (err) {
+      return res.status(403).send({message: 'Forbidden access'}) 
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
     try{
         await client.connect();
         const appointmentCollection = client.db('doctors').collection('appointment');
-        const bookingCollection = client.db('doctors').collection('booking')
+        const bookingCollection = client.db('doctors').collection('booking');
+        const userCollection = client.db('doctors').collection('users')
 
         app.get('/appointment', async(req, res) => {
             const query = {};
@@ -64,11 +82,32 @@ async function run() {
         })
 
         // my bookings in dashboard
-        app.get('/booking', async(req, res) => {
+        app.get('/booking', verifyJwt, async(req, res) => {
           const patientEmail = req.query.patientEmail;
-          const query = {patientEmail: patientEmail};
-          const bookings = await bookingCollection.find(query).toArray();
-          res.send(bookings);
+          const decodedEmail = req.decoded.email;
+          if(patientEmail === decodedEmail) {
+            const query = {patientEmail: patientEmail};
+            const bookings = await bookingCollection.find(query).toArray();
+            return res.send(bookings);
+          }
+          else{
+            return res.status(403).send({message: 'Forbifidden access'})
+          }
+        })
+
+
+        //api for users
+        app.put('/users/:email', async(req, res) => {
+          const email = req.params.email;
+          const filter = {email: email}
+          const options = { upsert: true };
+          const users = req.body;
+          const updateDoc = {
+            $set: users,
+          }; 
+          const result = await userCollection.updateOne(filter, updateDoc, options);
+          const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+          res.send({result, token})
         })
 
 
